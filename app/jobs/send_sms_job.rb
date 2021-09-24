73ce48dea15540c2ed22b0b1bfe361c4d47e89df
rex <rcsmie@gmail.com>
2021-09-23 23:55:50 -0700

    use env var for token + id

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -5,8 +5,8 @@ class SendSmsJob < ApplicationJob
 	end
 
 	def send_text(text, to)
-    account_sid = 'AC5e85d5c0d198f5b5c2888da1d286df72'
-    auth_token = '87b35d02b405db5ba66590a1d86bafd9'
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_TOKEN']
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
     from = '+17787847858'
